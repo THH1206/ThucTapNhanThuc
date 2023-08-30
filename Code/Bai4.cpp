5457052532d8d0c5a18484c26a26da424a9e5c71
Trần Hữu Hải <110327555+THH1206@users.noreply.github.com>
2023-08-30 21:06:57 +0700

    Bai4.cpp

--- Code/Bai4.cpp
+++ Code/Bai4.cpp
@@ -1,7 +1,6 @@
 //Liệt kê tất cả các “ước số” của số nguyên dương n
 #include<stdio.h>
 #include<conio.h>
-
 int main()
 {
     int i, n;
@@ -10,19 +9,15 @@ int main()
     printf("\nNhap n(n > 0): ");
     scanf("%d", &n);
       if(n <= 0)
-       {
         printf("\n N phai > 0. Xin nhap lai !");
-       }
     }while(n <= 0);
     i = 1;
-
     while(i <= n)
     {
         if(n % i == 0)
             printf("%4d", i);
             i++;
     }
-
     getch();
     return 0;
 }
